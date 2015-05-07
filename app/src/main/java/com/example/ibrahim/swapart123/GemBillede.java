79bcf77d1471d3902cfd1856c2113cf99ee61c85
kamranyusaf <kamiyusaf@hotmail.com>
2015-05-07 20:20:21 +0200

    Parse1

---
+++ app/src/main/java/com/example/ibrahim/swapart123/GemBillede.java
@@ -0,0 +1,20 @@
+package com.example.ibrahim.swapart123;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+/**
+ * Created by kamranyusaf on 07/05/15.
+ */
+public class GemBillede extends Activity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+    }
+    @Override
+    public void onClick(View v){
+
+    }
+}
